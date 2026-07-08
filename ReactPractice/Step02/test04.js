e547797b8d21de65622c9d68a16efdd336d204ea
Haedo <70150896+haedoz@users.noreply.github.com>
2026-07-08 15:36:50 +0900

    Delete ReactPractice directory

--- ReactPractice/Step02/test04.js
+++
@@ -1,4 +0,0 @@
-let [name, ...rest] = ["wonjang", 21, "Gyeongi"];
-
-console.log("name", name); // name wonjang
-console.log("rest", rest); // rest [ 21, 'Gyeongi' ]
--- ReactPractice/Step02/test05.js
+++
@@ -1,5 +0,0 @@
-let names = ["Steve", "John"];
-
-let students = ["Tom", ...names, ...names];
-
-console.log("students", students);
